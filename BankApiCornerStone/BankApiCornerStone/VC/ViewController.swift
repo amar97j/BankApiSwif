b7c98ee0349d895ac818fc69b8fca15e18ffb97e
amar97j <ajk.86@hotmail.com>
2024-03-11 12:19:45 +0300

    update

--- BankApiCornerStone/BankApiCornerStone/VC/ViewController.swift
+++ BankApiCornerStone/BankApiCornerStone/VC/ViewController.swift
@@ -8,26 +8,31 @@ class ViewController: UIViewController {
         imageView.image = UIImage(named: "nursery")
         imageView.contentMode = .scaleAspectFill
         imageView.clipsToBounds = true
-        imageView.layer.cornerRadius = 15
+        imageView.layer.cornerRadius = 20
         return imageView
     }()
 
     let blurEffectView: UIVisualEffectView = {
-        let blurEffect = UIBlurEffect(style: .light)
+        let blurEffect = UIBlurEffect(style: .systemUltraThinMaterialLight)
         let blurView = UIVisualEffectView(effect: blurEffect)
         return blurView
     }()
 
     let topContainerView: UIView = {
         let view = UIView()
+        view.layer.cornerRadius = 20
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.shadowOpacity = 0.5
+        view.layer.shadowOffset = CGSize(width: 0, height: 3)
+        view.layer.shadowRadius = 10
         return view
     }()
 
     let topLabel: UILabel = {
         let label = UILabel()
-        label.text = "Welcome to NURSERIA App"
-        label.textColor = UIColor(red: 102/255, green: 204/255, blue: 255/255, alpha: 1.0)
-        label.font = UIFont.systemFont(ofSize: 28, weight: .semibold)
+        label.text = "Welcome to NURSERIA"
+        label.textColor = UIColor(red: 41/255, green: 128/255, blue: 185/255, alpha: 1.0)
+        label.font = UIFont.systemFont(ofSize: 36, weight: .bold)
         label.textAlignment = .center
         return label
     }()
@@ -37,7 +42,7 @@ class ViewController: UIViewController {
         imageView.image = UIImage(named: "nursery")
         imageView.contentMode = .scaleAspectFill
         imageView.clipsToBounds = true
-        imageView.layer.cornerRadius = 10
+        imageView.layer.cornerRadius = 15
         return imageView
     }()
     
@@ -45,8 +50,12 @@ class ViewController: UIViewController {
         let button = UIButton()
         button.setTitle("Sign In", for: .normal)
         button.setTitleColor(.white, for: .normal)
-        button.backgroundColor = .systemBlue
+        button.backgroundColor = UIColor(red: 52/255, green: 152/255, blue: 219/255, alpha: 1.0)
         button.layer.cornerRadius = 12
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.5
+        button.layer.shadowOffset = CGSize(width: 0, height: 3)
+        button.layer.shadowRadius = 5
         button.addTarget(self, action: #selector(signInButtonTapped), for: .touchUpInside)
         return button
     }()
@@ -54,7 +63,7 @@ class ViewController: UIViewController {
     let signUpLabel: UILabel = {
         let label = UILabel()
         label.text = "Don't have an account? Sign Up"
-        label.textColor = .systemBlue
+        label.textColor = .darkGray
         label.font = UIFont.systemFont(ofSize: 16, weight: .medium)
         label.textAlignment = .center
         label.isUserInteractionEnabled = true
@@ -83,9 +92,9 @@ class ViewController: UIViewController {
 
         topContainerView.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
-            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(20)
-            make.width.equalTo(200)
-            make.height.equalTo(220)
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(40)
+            make.width.equalTo(300)
+            make.height.equalTo(320)
         }
 
         topLabel.snp.makeConstraints { make in
@@ -95,14 +104,14 @@ class ViewController: UIViewController {
 
         imageView.snp.makeConstraints { make in
             make.centerX.equalTo(topContainerView)
-            make.top.equalTo(topLabel.snp.bottom).offset(16)
-            make.width.height.equalTo(150)
+            make.top.equalTo(topLabel.snp.bottom).offset(40)
+            make.width.height.equalTo(200)
         }
 
         view.addSubview(signInButton)
         signInButton.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
-            make.centerY.equalToSuperview()
+            make.top.equalTo(imageView.snp.bottom).offset(40)
             make.width.equalTo(200)
             make.height.equalTo(50)
         }
